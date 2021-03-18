018e56440b4f4b857a28b60f09a2f81208b6b362
Marieta28 <69346003+Marieta28@users.noreply.github.com>
2021-03-18 21:55:20 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $('#app').vide({
     mp4: 'video/video.mp4',
     webm: 'video/video.webm',
-    ogv: 'video/video.ogv'
+    ogv: 'video/video.ogv',
+    poster: 'img/black.jpg'
   });
 });
